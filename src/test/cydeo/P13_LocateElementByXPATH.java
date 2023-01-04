905d94014509c795fcf9f5b41da016c1c60ebbf0
Bill Glory <billglory90@gmail.com>
2023-01-04 19:33:14 +0300

    TestsSelenium

---
+++ src/test/cydeo/P13_LocateElementByXPATH.java
@@ -0,0 +1,36 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class P13_LocateElementByXPATH {
+
+    public static void main(String[] args) {
+
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.google.com");
+
+//search for cydeo
+        WebElement searchBox = driver.findElement(By.xpath("//input[@name='q']"));
+        searchBox.sendKeys("Cydeo"+ Keys.ENTER);
+
+
+        WebElement cydeoText = driver.findElement(By.xpath("//h3[.='Cydeo']"));
+
+        if (cydeoText.getText().equals("Cydeo")) {
+            System.out.println("Cydeo text is appeared");
+        } else {
+            System.out.println("Cydeo text is no failed");
+        }
+
+driver.quit();
+
+    }
+}
