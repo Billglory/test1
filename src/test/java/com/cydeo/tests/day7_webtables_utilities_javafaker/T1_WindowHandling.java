b8fa6714008f02ec59907cf67aedcbe396eaa2c9
Bill Glory <billglory90@gmail.com>
2023-01-11 12:47:12 +0300

    day7:webtablesutilitiesjavafaker;

---
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
@@ -0,0 +1,64 @@
+package com.cydeo.tests.day7_webtables_utilities_javafaker;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class T1_WindowHandling {
+
+
+    public WebDriver driver;
+
+    @BeforeMethod
+
+    public void setupMethod() {
+
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+
+    public void window_handling_test() {
+        driver.get("https://www.amazon.com");
+
+
+        //2. Go to: https://www.amazon.com
+
+        //3. Copy-paste the lines from below into your class
+        ((JavascriptExecutor) driver).executeScript("window.open('https://google.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://etsy.com','_blank');");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
+
+
+        //4. Create a logic to switch to the tab where Etsy.com is open
+
+        Set<String> allWindowHandles = driver.getWindowHandles();
+        for (String each : allWindowHandles) {
+            driver.switchTo().window(each);
+            System.out.println("Current :" + driver.getCurrentUrl());
+        }
+    }
+
+}
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+        BrowserUtils.switchWindowAndVerify(driver, "etsy", "Etsy");
+    }
+ */
