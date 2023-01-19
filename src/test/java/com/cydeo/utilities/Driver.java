a4c040ddd49b6f899ed4f545f05a584253875d1b
Bill Glory <billglory90@gmail.com>
2023-01-19 12:18:15 +0300

    day9:Driver Java

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -41,6 +41,7 @@ public class Driver {
                 Depending on the browserType that will be return from configuration.properties file
                 switch statement will determine the case, and open the matching browser
             */
+
             switch (browserType) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
